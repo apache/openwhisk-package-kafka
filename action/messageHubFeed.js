d6a7905f6479105e322054919145c51a579359ae
Justin Berstler <bjustin-ibm@users.noreply.github.com>
2016-11-10 16:52:26 +0000

    Check Message Hub credentials before creating feed (#49)
    Use the Message Hub REST API to list existing topics - this verifies the credentials are correct
    While we're at it, make sure the topic they want to subscribe to already exists.

--- action/messageHubFeed.js
+++ action/messageHubFeed.js
@@ -29,20 +29,27 @@ function main(params) {
             return;
         }
 
-        var body = validatedParams;
-        body.triggerURL = 'https://' + whisk.getAuthKey() + "@" + params.endpoint + '/api/v1/namespaces/' + namespace + '/triggers/' + trigger;
+        // make sure we have valid MH credentials
+        console.log("Checking Message Hub credentials...");
+        return checkMessageHubCredentials(validatedParams)
+            .then(function() {
+                console.log("Successfully authenticated with Message Hub");
 
-        var options = {
-            method: 'PUT',
-            url: feedServiceURL,
-            body: JSON.stringify(body),
-            headers: {
-                'Content-Type': 'application/json',
-                'User-Agent': 'whisk'
-            }
-        };
+                var body = validatedParams;
+                body.triggerURL = 'https://' + whisk.getAuthKey() + "@" + params.endpoint + '/api/v1/namespaces/' + namespace + '/triggers/' + trigger;
 
-        return doRequest(options);
+                var options = {
+                    method: 'PUT',
+                    url: feedServiceURL,
+                    body: JSON.stringify(body),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'User-Agent': 'whisk'
+                    }
+                };
+
+                return doRequest(options);
+            });
     } else if (params.lifecycleEvent === 'DELETE') {
         var authorizationHeader = 'Basic ' + new Buffer(whisk.getAuthKey()).toString('base64');
 
@@ -60,6 +67,34 @@ function main(params) {
     }
 }
 
+function checkMessageHubCredentials(params) {
+    // listing topics seems to be the simplest way to check auth
+    var topicURL = params.kafka_admin_url + '/admin/topics';
+
+    var options = {
+        method: 'GET',
+        url: topicURL,
+        headers: {
+            'X-Auth-Token': params.api_key
+        }
+    };
+
+    return doRequest(options)
+        .then(function(body) {
+            console.log("Successfully authenticated with Message Hub");
+
+            var topicNames = body.response.map(function(topic) {
+                return topic.name
+            });
+
+            if(topicNames.indexOf(params.topic) < 0) {
+                return Promise.reject('Topic does not exist. You must create the topic first: ' + params.topic);
+            };
+        }, function(authError) {
+            return Promise.reject('Could not authenticate with Message Hub. Please check your credentials.');
+        });
+}
+
 function doRequest(options) {
     var promise = new Promise(function (resolve, reject) {
         request(options, function (error, response, body) {
@@ -73,7 +108,7 @@ function doRequest(options) {
                 console.log("Status code: " + response.statusCode);
 
                 if (response.statusCode >= 400) {
-                    console.log("Response from Message Hub feed service: " + body);
+                    console.log("Response: " + JSON.stringify(body));
                     reject({
                         statusCode: response.statusCode,
                         response: JSON.parse(body)
@@ -122,6 +157,20 @@ function validateParameters(rawParams) {
         }
     } else {
         whisk.error('You must supply a "kafka_brokers_sasl" parameter as an array of Message Hub brokers.');
+        return;
+    }
+
+    if(rawParams.kafka_admin_url) {
+        validatedParams.kafka_admin_url = rawParams.kafka_admin_url;
+    } else {
+        whisk.error('You must supply a "kafka_admin_url" parameter.');
+        return;
+    }
+
+    if(rawParams.api_key) {
+        validatedParams.api_key = rawParams.api_key;
+    } else {
+        whisk.error('You must supply an "api_key" parameter.');
         return;
     }
 
