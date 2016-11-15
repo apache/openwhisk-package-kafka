bb4625f0b7a0c2068c7a5e75e74e3512dcbe2f10
Justin Berstler <bjustin-ibm@users.noreply.github.com>
2016-11-15 18:04:29 +0000

    Actively monitor consumers - attempt to heal sick ones individually (#54)
    * Implement "The Doctor"
    
    The Doctor is a separate thread that monitors the health of consumers.  If it notices that a consumer is sick (should be running, but isn't) it will attempt to restart that consumer.
    
    This required making a variety of classes and data structures thread safe, as they may be accessed by the Doctor and the web app simultaneously. "currentState" reflects how the consumer believes it is running, while "desiredState" is the one the consumer should actively work toward transitioning to. "desiredState" can only be set to Restart or Dead.
    
    This also required implementing explicit states in Consumers that allow them to be restarted gracefully. The biggest change here is that the Consumer no longer *is* a Thread, but instead has a field which is the ConsumerThread.
    
    * Adjust logging
    
    Change logging levels on a variety of messages - mostly changing info to debug
    
    * Keep track of how many times a Consumer is restarted
    
    This is more for informational purposes, and isn't necessarily actionable in an automated way.
    
    * Automatically restart consumers that haven't polled in a while
    
    There seems to be an issue where the kafka-python client gets into an error-handling loop. This causes poll() to not succeed, but also not throw an exception either.
    
    This code adds logic to the Doctor to detect when a Consumer has not successfully polled in a while (2 seconds) and automatically restarts it.
    
    Thanks @dubeejw

---
+++ provider/thedoctor.py
@@ -0,0 +1,46 @@
+import logging
+import time
+
+from consumer import Consumer
+from consumercollection import ConsumerCollection
+from threading import Thread
+
+class TheDoctor (Thread):
+    # maximum time to allow a consumer to not successfully poll() before restarting
+    poll_timeout_seconds = 2
+
+    # interval between the Doctor making rounds
+    sleepy_time_seconds = 2
+
+    def __init__(self, consumerCollection):
+        Thread.__init__(self)
+
+        self.daemon = True
+        self.consumerCollection = consumerCollection
+
+    def run(self):
+        logging.info('[Doctor] The Doctor is in!')
+
+        while True:
+            consumers = self.consumerCollection.getCopyForRead()
+
+            for consumerId in consumers:
+                consumer = consumers[consumerId]
+                logging.debug('[Doctor] [{}] Consumer is in state: {}'.format(consumerId, consumer.currentState()))
+
+                if consumer.currentState() is Consumer.State.Dead and consumer.desiredState() is Consumer.State.Running:
+                    # well this is unexpected...
+                    logging.error('[Doctor][{}] Consumer is dead, but should be alive!'.format(consumerId))
+                    consumer.restart()
+                elif consumer.currentState() is Consumer.State.Dead and consumer.desiredState() is Consumer.State.Dead:
+                    # Bring out yer dead...
+                    logging.info('[{}] Removing dead consumer from the collection.'.format(consumer.trigger))
+                    self.consumerCollection.removeConsumerForTrigger(consumer.trigger)
+                elif consumer.secondsSinceLastPoll() > self.poll_timeout_seconds and consumer.desiredState() is Consumer.State.Running:
+                    # there seems to be an issue with the kafka-python client where it gets into an
+                    # error-handling loop. This causes poll() to never complete, but also does not
+                    # throw an exception.
+                    logging.error('[Doctor][{}] Consumer timed-out, but should be alive! Restarting consumer.'.format(consumerId))
+                    consumer.restart()
+
+            time.sleep(self.sleepy_time_seconds)
