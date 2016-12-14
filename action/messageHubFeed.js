ab7f90028ff6e5d3a2fbccabf8c6a2598ad6035b
Justin Berstler <bjustin@us.ibm.com>
2016-12-14 13:09:26 -0500

    Review feedback
    Use regex to more correctly strip out the optional protocol.

--- action/messageHubFeed.js
+++ action/messageHubFeed.js
@@ -44,7 +44,7 @@ function main(params) {
 
                 // params.endpoint may already include the protocol - if so,
                 // strip it out
-                var massagedAPIHost = massageAPIHost(params.endpoint);
+                var massagedAPIHost = params.endpoint.replace(/https?:\/\/(.*)/, "$1");
                 body.triggerURL = 'https://' + whisk.getAuthKey() + "@" + massagedAPIHost + '/api/v1/namespaces/' + namespace + '/triggers/' + trigger;
 
                 var options = {
@@ -201,14 +201,4 @@ function validateBrokerParam(brokerParam) {
 
 function isNonEmptyArray(obj) {
     return obj && Array.isArray(obj) && obj.length !== 0;
-}
-
-// if the apiHost already includes the protocol, remove it
-function massageAPIHost(apiHost) {
-    if(apiHost.substr(0, 4) === 'http') {
-        // the apiHost includes the protocol - strip it out
-        return apiHost.split('/')[2]
-    } else {
-        return apiHost;
-    }
 }
