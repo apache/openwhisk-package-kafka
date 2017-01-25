d096036e86f55f158714e1f9cd81591b11abf03f
Justin Berstler <bjustin@us.ibm.com>
2017-01-25 13:58:16 -0500

    Consolidate Message Hub initialization into KafkaUtils
    Intializes JVM with required parameters for MH authentication
    Gets/parses VCAP file with MH parameters

--- tests/src/system/health/MessagingFeedTests.scala
+++ tests/src/system/health/MessagingFeedTests.scala
@@ -16,15 +16,11 @@
 
 package system.health
 
-import java.util.HashMap
-import java.util.Properties
-import javax.security.auth.login.Configuration
-import javax.security.auth.login.AppConfigurationEntry
+import system.utils.KafkaUtils
 
 import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
 
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.runner.RunWith
 import org.scalatest.BeforeAndAfterAll
@@ -34,14 +30,11 @@ import org.scalatest.junit.JUnitRunner
 
 import common.JsHelpers
 import common.TestHelpers
-import common.TestUtils
 import common.Wsk
 import common.WskActorSystem
 import common.WskProps
 import common.WskTestHelpers
-import spray.json.DefaultJsonProtocol.StringJsonFormat
-import spray.json.JsArray
-import spray.json.JsString
+import spray.json.DefaultJsonProtocol._
 import spray.json.pimpAny
 
 @RunWith(classOf[JUnitRunner])
@@ -63,59 +56,23 @@ class MessagingFeedTests
     val messagingPackage = "/whisk.system/messaging"
     val messageHubFeed = "messageHubFeed"
 
-    def setMessageHubSecurityConfiguration(user: String, password: String) = {
-        val map = new HashMap[String, String]()
-        map.put("serviceName", "kafka")
-        map.put("username", user)
-        map.put("password", password)
-        Configuration.setConfiguration(new Configuration()
-        {
-            def getAppConfigurationEntry(name: String): Array[AppConfigurationEntry] = Array(
-    	          new AppConfigurationEntry (
-    	              "com.ibm.messagehub.login.MessageHubLoginModule",
-     			          AppConfigurationEntry.LoginModuleControlFlag.REQUIRED, map))
-        })
-    }
+    val kafkaUtils = new KafkaUtils
 
     behavior of "Message Hub"
 
     it should "fire a trigger when a message is posted to the message hub" in withAssetCleaner(wskprops) {
-        var credentials = TestUtils.getCredentials("message_hub")
-        val user = credentials.get("user").getAsString()
-        val password = credentials.get("password").getAsString()
-        val kafka_admin_url = credentials.get("kafka_admin_url").getAsString()
-        val api_key = credentials.get("api_key").getAsString()
-        val kafka_brokers_sasl_json_array = credentials.get("kafka_brokers_sasl").getAsJsonArray()
-
-        var vec = Vector[JsString]()
-        var servers = s""
-        val iter = kafka_brokers_sasl_json_array.iterator();
-        while(iter.hasNext()){
-            val server = iter.next().getAsString()
-            vec = vec :+ JsString(server)
-            servers = s"$servers$server,"
-        }
-        var kafka_brokers_sasl = JsArray(vec)
-
         val currentTime = s"${System.currentTimeMillis}"
-        System.setProperty("java.security.auth.login.config", "")
-        setMessageHubSecurityConfiguration(user, password)
-        var props = new Properties()
-        props.put("bootstrap.servers", servers);
-        props.put("security.protocol", "SASL_SSL");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         (wp, assetHelper) =>
             val triggerName = s"/_/dummyMessageHubTrigger-$currentTime"
             val feedCreationResult = assetHelper.withCleaner(wsk.trigger, triggerName) {
                 (trigger, _) =>
                     trigger.create(triggerName, feed = Some(s"$messagingPackage/$messageHubFeed"), parameters = Map(
-                        "user" -> user.toJson,
-                        "password" -> password.toJson,
-                        "api_key" -> api_key.toJson,
-                        "kafka_admin_url" -> kafka_admin_url.toJson,
-                        "kafka_brokers_sasl" -> kafka_brokers_sasl,
+                        "user" -> kafkaUtils("user").asInstanceOf[String].toJson,
+                        "password" -> kafkaUtils("password").asInstanceOf[String].toJson,
+                        "api_key" -> kafkaUtils("api_key").asInstanceOf[String].toJson,
+                        "kafka_admin_url" -> kafkaUtils("kafka_admin_url").asInstanceOf[String].toJson,
+                        "kafka_brokers_sasl" -> kafkaUtils("brokers").asInstanceOf[List[String]].toJson,
                         "topic" -> topic.toJson))
             }
             withActivation(wsk.activation, feedCreationResult, initialWait = 5 seconds, totalWait = 60 seconds) {
@@ -127,13 +84,15 @@ class MessagingFeedTests
             // It takes a moment for the consumer to fully initialize. We choose 2 seconds
             // as a temporary length of time to wait for.
             Thread.sleep(2000)
-            val producer = new KafkaProducer[String, String](props)
+
+            val producer = kafkaUtils.createProducer()
             val record = new ProducerRecord(topic, "key", currentTime)
             producer.send(record)
             producer.close()
             val activations = wsk.activation.pollFor(N = 2, Some(triggerName), retries = 30)
             var triggerFired = false
             assert(activations.length > 0)
+
             for (id <- activations) {
                 val activation = wsk.activation.waitForActivation(id)
                 if (activation.isRight) {
