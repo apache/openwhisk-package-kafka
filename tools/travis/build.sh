2dc87c66e6c2b6c883dd8337c41937b73fa2324a
dubeejw <jwdubee@us.ibm.com>
2017-02-02 10:32:03 -0500

    Add Travis CI
    - Add Travis CI to run scanCode.py
    
    Add Execution Permissions to Sh Files

---
+++ tools/travis/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Build script for Travis-CI.
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../.."
+WHISKDIR="$ROOTDIR/../openwhisk"
+
+# Scan code
+
+cd $WHISKDIR
+tools/build/scanCode.py $ROOTDIR
