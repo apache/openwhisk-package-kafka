3bb44e764e6e1aedeb1c94328e7c5cc45ee2736e
dubeejw <jwdubee@us.ibm.com>
2019-11-18 21:19:58 -0500

    Pass correct argument to disable

--- action/lib/Database.js
+++ action/lib/Database.js
@@ -60,7 +60,7 @@ module.exports = function(dbURL, dbName) {
                 if(err) {
                     if(err.statusCode && err.statusCode === 409) {
                         this.getTrigger(params.triggerName)
-                        .then(() => this.disableTrigger(params.triggerName))
+                        .then(doc => this.disableTrigger(doc))
                         .then(() => this.getTrigger(params.triggerName))
                         .then(doc => this.updateTrigger(doc, params))
                         .then(result => resolve(result))
