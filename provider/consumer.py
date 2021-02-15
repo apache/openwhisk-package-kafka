943a63ab6cde1dcc9801c88b6a81b37e7287f735
steven0711dong <53186782+steven0711dong@users.noreply.github.com>
2021-02-15 17:23:27 -0500

    unique client.id for each region (#46)

--- provider/consumer.py
+++ provider/consumer.py
@@ -44,6 +44,7 @@ payload_limit = int(os.getenv('PAYLOAD_LIMIT', 900000))
 check_ssl = (local_dev == 'False')
 cloud_functions_stamp = "-CFn"
 cfn_pipeline_test = "cfnpipelinetest"
+kafka_client_Id = os.getenv('KAFKA_CLIENT_ID', 'cfn')
 seconds_in_day = 86400
 non_existent_topic_status_code = 404
 invalid_credential_status_code = 403
@@ -349,6 +350,7 @@ class ConsumerProcess (Process):
             cg = self.topic + cloud_functions_stamp if cfn_pipeline_test in self.trigger else self.trigger
             config = {'metadata.broker.list': ','.join(self.brokers),
                         'group.id': cg,
+                        'client.id': kafka_client_Id,
                         'default.topic.config': {'auto.offset.reset': 'latest'},
                         'enable.auto.commit': False,
                         'api.version.request': True
